ca794e1bc9b08392f23b2c6045f7dff1730e54a4
zamaneiffat@outlook.com <zamaneiffat@outlook.com>
2020-03-15 09:05:36 -0700

    commit

---
+++ ASPNetCoreIdentity/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreIdentity.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public Class Class { get; set; } 
+    }
+    public enum Class
+    {
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten
+    };
+}
